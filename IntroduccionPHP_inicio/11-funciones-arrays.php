45122d193750eef998d1738ef2dc56898064e790
Da.B <da.b@hotmail.es>
2021-03-03 12:54:31 +0100

    anotha

--- IntroduccionPHP_inicio/11-funciones-arrays.php
+++ IntroduccionPHP_inicio/11-funciones-arrays.php
@@ -1,6 +1,45 @@
 <?php include 'includes/header.php';
 
 
+// in_array - buscar elementos en un array
+
+$carrito = ['Tablet', 'Computadora', 'Television'];
+
+var_dump( in_array('Tablet', $carrito) );
+var_dump( in_array('Polla', $carrito) );
+
+//Ordenar elementos en un array
+$numeros = array(1,2,3,4,5,1,2);
+sort($numeros);  // de menor a mayor
+echo "<pre>";
+
+var_dump($numeros);
+echo "</pre>";
+rsort($numeros); // de mayor a menor
+
+echo "<pre>";
+var_dump($numeros);
+echo "</pre>";
+
+// Ordenar array asociativo
+$cliente = array(
+    'saldo' => 200,
+    'tipo' => 'Premium',
+    'nombre' => 'David'
+);
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+asort($cliente);  // Ordena por valores
+ksort($cliente); // ordena por llaves (orden alfabetico a-z)
+krsort($cliente); // ordena por llaves (orden alfabetico a-z)
+arsort($cliente); // ordena por valores, z primero
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 
 
--- IntroduccionPHP_inicio/12-condicionales.php
+++ IntroduccionPHP_inicio/12-condicionales.php
@@ -1,7 +1,71 @@
 <?php include 'includes/header.php';
 
 
+$login = true;
+$admin = true;
+
+if($login && $admin) {
+  echo "Usuario Login Ok";   
+} else {
+    echo "No estas logeado, incia sesion cabronazo";
+}
+
+echo "<br>";
+
+// If anidados
+
+$cliente = [
+    'nombre' => 'David',
+    'saldo' => 0,
+    'informacion' => [
+        'tipo' => 'Regular'
+    ]
+];
 
 
 
+if(!empty($cliente)) {
+    echo "El array de cliente NO está vacio";
+    echo "<br>";
+
+    if($cliente['saldo'] > 0) {
+        echo "El cliente tiene saldo disponible";
+        echo "<br>";
+    } else {
+        echo "No hay saldo, estás más tieso que la mojama";
+        echo "<br>";
+    }
+} 
+
+echo "<br>";
+// Else If
+
+if($cliente['saldo'] > 0) {
+    echo "El cliente tiene saldo";
+} else if ($cliente['informacion'] ['tipo'] === 'Premium') {
+    echo " Cliente Premium";
+} else {
+    echo "No hay cliente definido o no es premium";
+}
+
+
+echo "<br>";
+//Switch 
+
+$tecnologia = 'HTML';
+
+switch ($tecnologia) {
+    case 'PHP':
+        echo "PHP, excelente lenguaje!";
+        break;
+    case 'JavasCript':
+        echo "Eres el puto amo del JS";
+        break;
+    case 'HTML':
+        echo "Algun tonto tenia que haber";
+        break;    
+    default:
+        echo "Algún lenguage que no sabemos cual es";
+        break;
+}
 include 'includes/footer.php';
